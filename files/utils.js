132211ca0bd7a6e4ba6540d9e5ccdcd8d8257a20
kk <kk@efort.com.ua>
2012-08-10 16:34:00 +0300

    cube added

--- files/utils.js
+++ files/utils.js
@@ -6,3 +6,25 @@ function getSize() {
 		width  : el.width
 	};
 }
+
+function extend(gl) {
+
+	gl.begin2D = function() {
+
+		gl.matrixMode(gl.PROJECTION);
+		gl.pushMatrix();
+		gl.loadIdentity();
+		gl.ortho(0, gl.drawingBufferWidth, gl.drawingBufferHeight, 0, -1.0, 1.0);
+		gl.matrixMode(gl.MODELVIEW);
+		gl.pushMatrix();
+		gl.loadIdentity();
+	}
+
+	gl.end2D = function() {
+		gl.matrixMode(gl.PROJECTION);
+		gl.popMatrix();
+		gl.matrixMode(gl.MODELVIEW);
+		gl.popMatrix();
+	}
+
+}
