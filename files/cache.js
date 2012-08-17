38b026861b2bd09cf10846cb34494f354a2274e6
klimaka <klimaka@ubuntu.(none)>
2012-08-17 14:09:33 +0400

    more refactoring

--- files/cache.js
+++ files/cache.js
@@ -2,42 +2,40 @@
 var Cache = {
 
 
-cache : {},
-cacheListeners : {},
-lastTmp : 0,
+	cache : {},
+	cacheListeners : {},
+	lastTmp : 0,
 
-load : function(file, onload) {
+	load : function(file, onload) {
 
-	if (Cache.cache[file] != null) {
-		onload(Cache.cache[file]);
-		return;
+		if (Cache.cache[file] != null) {
+			onload(Cache.cache[file]);
+			return;
+		}
+		
+		if (Cache.cacheListeners[file] != undefined) {
+			Cache.cacheListeners[file].push(onload);
+			return;
+		}
+
+		var tmpID = Cache.lastTmp++;
+		var head = document.getElementsByTagName('head')[0];
+		var script = document.createElement('script');
+		script.src = '/file_load/id'+tmpID+'/' + file;
+		script.type = 'text/javascript';
+		Cache.cache[file] = null;
+		Cache.cacheListeners[file] = [onload];
+
+		script.onload = function() {
+			var val = window['id'+tmpID];
+			delete window['id'+tmpID];
+			Cache.cache[file] = val;
+			var listeners = Cache.cacheListeners[file];
+			for (i in listeners) listeners[i](val);
+			delete Cache.cacheListeners[file];
+		}
+
+		head.appendChild(script);
+		Cache.cache[file];
 	}
-	
-	if (Cache.cacheListeners[file] != undefined) {
-		Cache.cacheListeners[file].push(onload);
-		return;
-	}
-
-	var tmpID = Cache.lastTmp++;
-	var head = document.getElementsByTagName('head')[0];
-	var script = document.createElement('script');
-	script.src = '/file_load/id'+tmpID+'/' + file;
-	script.type = 'text/javascript';
-	Cache.cache[file] = null;
-	Cache.cacheListeners[file] = [onload];
-
-	script.onload = function() {
-		var val = window['id'+tmpID];
-		delete window['id'+tmpID];
-		Cache.cache[file] = val;
-		var listeners = Cache.cacheListeners[file];
-		for (i in listeners) listeners[i](val);
-		delete Cache.cacheListeners[file];
-	}
-
-	head.appendChild(script);
-	Cache.cache[file];
-}
-
-
 }
---
+++ files/shaders/drawer.js
@@ -0,0 +1,105 @@
+
+var Scene = {
+
+	defaultGLShader : null,
+
+	Shader : function(shader) {
+
+		this.glShader =  Scene.defaultGLShader 
+			         || (Scene.defaultGLShader = new GL.Shader('void main(){gl_Position = vec4(0,0,0,0);}', 'void main(){gl_FragColor = vec4(0,0,0,0);}'));
+
+		var v = null, f = null;
+		Cache.load('shaders/'+shader+'_v.glsl', function(d){v = d; if (f) this.load(v,f);});
+		Cache.load('shaders/'+shader+'_f.glsl', function(d){f = d; if (v) this.load(v,f);});
+	},
+
+	Drawer : function() {
+
+		this.ents = {};
+		this.shaders = {};
+	},
+
+	ObjectIterator : function(objs) {
+		this.objs = objs;
+		this.idxs = Object.keys(this.objs);
+		this.current = 0;
+		this.current_obj = this.objs[this.idxs[0]];
+		this.end = this.current_obj == undefined;
+	}
+}
+
+Scene.ObjectIterator.prototype = {
+
+	get : function() {
+		return this.current_obj;
+	},
+
+	next : function() {
+
+		if (this.end) return null;
+
+		if (this.current == this.idxs.lenght - 1) {
+			this.end = true;
+			this.current_obj = null;
+			return null;
+		}
+
+		this.current_obj = this.objs[this.idxs[this.current++]];
+		return this.current_obj;
+	},
+
+	reset : function() {
+		this.idxs = Object.keys(this.objs);
+		this.current = 0;
+		this.current_obj = this.objs[this.idxs[0]];
+		this.end = this.current_obj == undefined;
+	},
+}
+
+Scene.Shader.prototype = {
+
+	load : function(vsh, fsh) {
+		this.glShader = new GL.Shader(vsh, fsh);
+	},
+
+	activate : function(options) {
+		if (options.pre != undefined)
+			this.glShader.uniforms(options.pre);
+	},
+
+	deactivate : function(options) {
+		if (options.post != undefined)
+			this.glShader.uniforms(options.post);
+	},
+
+	draw : function(obj, options) {
+
+		var per = options.per;
+		if (per) {
+			for(i in per) per[i] = obj.uniforms[i];
+			this.glShader.uniforms(per);
+		}
+		this.glShader.draw(obj.mesh, options.mode);
+	},
+}
+
+Scene.Drawer.prototype = {
+
+	loadShader : function(file) {
+		this.shaders[file] = new Scene.Shader(shader);
+	},
+
+	objIterator : function() {
+	}
+
+	draw : function(shaderName, options) {
+
+		var shader = this.shaders[shaderName];
+		shader.activate(options);
+		for (i in this.ents) {
+			shader.draw(this.ents[i], options);
+		}
+		shader.deactivate(options);
+	},
+
+}
