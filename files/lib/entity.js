fbf9e534915730b17cac6a7fafa4a7656147d864
kk <kk@efort.com.ua>
2012-08-27 13:18:34 +0300

    new wirebuilder

--- files/lib/entity.js
+++ files/lib/entity.js
@@ -57,12 +57,12 @@ Entity.WireBuilder.prototype = {
 		this.lastIdx += 2;
 	},
 
-	buildGrid : function(n) {
+	buildGrid : function(x1, y1, x2, y2, n) {
 
-		var x = 0, dx = 1 / n, y = 0, dy = 1 /n;
+		var x = 0, dx = (x2-x1)/n, y = y1, dy = (y2-y1)/n;
 		for(var i = 0; i <= n; i++) {
-			this.addLine(i*dx, 0, 0, i*dx, 0, 1);
-			this.addLine(0, 0, i*dy, 1, 0, i*dy);
+			this.addLine(x1 + i*dx, 0, y1, x1 + i*dx, 0, y2);
+			this.addLine(x1, 0, y1 + i*dy, x2, 0, y1 + i*dy);
 		}
 
 		return this.compile();
