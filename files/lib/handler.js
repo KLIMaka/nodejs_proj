c1072ead76f93ec6a1f1597d3cc2a05d65f082fd
kk <kk@efort.com.ua>
2012-09-03 12:55:50 +0300

    more handler

--- files/lib/handler.js
+++ files/lib/handler.js
@@ -1,20 +1,47 @@
 
-function AxisHandle(axis, angles, color) {
+function AxisHandle(handler, axis, angles, color) {
 
 	this.axis = axis;
 	this.handle = new Entity.Model('models/pivot.obj');
-	this.setAnglesVec(angles);
-	this.uniforms.color = color;
+	this.handle.setAnglesVec(angles);
+	this.handle.uniforms.color = color;
+
+	this.handler = handler;
 }
 
 AxisHandle.prototype = {
 
-	connect : function() {
+	update : function(ent) {
 		this.handle.setPosVec(ent.position.add(this.axis));
-	}
+	},
+
+	onmousedown : function(e) {
+
+		var tracer = new GL.Raytracer();
+        var ray = tracer.getRayForPixel(e.x, e.y);
+        this.handler.hit = GL.Raytracer.hitTestMesh(tracer.eye, ray, this.handle).hit;
+        this.handler.originalOffset = this.handle.position.clone();
+	},
+
+	drag : function(e) {
+
+		var original_hit = this.handler.hit;
+        var tracer = new GL.Raytracer();
+        var ray = tracer.getRayForPixel(e.x, e.y);
+        var t = original_hit.subtract(tracer.eye).dot(this.axis) / ray.dot(this.axis);
+        var hit = tracer.eye.add(ray.multiply(t));
+        var offset = originalOffset.add(hit.subtract(original_hit));
+        this.handler.ent.setPosVec(GL.Vector.snap(offset.subtract(this.axis), 1));
+        this.handler.update();
+	},
 }
 
 function Handler() {
 
 	this.x = 
+}
+
+Handler.prototype = {
+
+
 }
