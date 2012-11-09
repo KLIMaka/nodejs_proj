52392c5e181ba3300f56412fcdf9c33b8fdf626a
kk <kk@efort.com.ua>
2012-11-09 17:54:01 +0200

    model && list changes

--- files/lib/model.js
+++ files/lib/model.js
@@ -3,6 +3,15 @@ Namespace('Model.Vertex', Math2D.Vertex.extend({
 
 	construct : function(x, y) {
 		Math2D.Vertex.construct.call(this, x, y);
+		this.adjSegments = Utils.list.create();
+	},
+
+	connectToSegment : function(segment) {
+		this.adjSegments.insert(segment);
+	},
+
+	disconnectFromSegment : function(segment) {
+		this.adjSegments.remove(segment);
 	},
 
 }));
@@ -13,6 +22,14 @@ Namespace('Model.Segment', Math2D.Segment.extend({
 		Math2D.Segment.construct.call(this, start, end);
 		this.front = front;
 		this.back = back;
+
+		this.start.connectToSegment(this);
+		this.end.connectToSegment(this);
+	},
+
+	remove : function() {
+		this.start.disconnectFromSegment(this);
+		this.end.disconnectFromSegment(this);
 	},
 
 }));
@@ -66,16 +83,22 @@ Namespace('Model.Level', Class.extend({
 
 		var seg_idx = this.segmentIntersection(vertex);
 		if (seg_idx != null) {
-			var seg = this.segments.get[seg_idx];
+			var seg = this.segments.get(seg_idx);
 			var a = Math2D.Segment.create(seg.start, vertex, seg.front, seg.back);
 			var b = Math2D.Segment.create(vertex, seg.end, seg.front, seg.back);
 
+			seg.remove();
 			this.segments.remove(seg_idx);
+
 			this.segments.add(a);
 			this.segments.add(b);
 		}
 
 		return true;
+	},
+
+	weld : function(v1, v2) {
+
 	},
 
 
--- files/lib/utils/list.js
+++ files/lib/utils/list.js
@@ -2,7 +2,13 @@
 Namespace('Ulils.List', Class.extend({
 
 	construct : function(obj) {
-		this.obj = obj;
+		this.obj = obj == undefined ? null : obj;
+		this.prev = null;
+		this.next = null;
+	},
+
+	unlink : function() {
+		this.obj = null;
 		this.prev = null;
 		this.next = null;
 	},
@@ -49,8 +55,34 @@ Namespace('Ulils.List', Class.extend({
 		prev.linkNext(list);
 	},
 
+	insert : function(obj) {
+		if (this.obj == null) {
+			this.obj = obj;
+		} else {
+			this.insertAfter(Ulils.List.create(obj));
+		}
+	},
+
+	remove : function(obj) {
+		var node = this;
+		while (node != null && node.obj !== obj) node = node.next;
+		if (node != null && node !== this) {
+			node.prev.linkNext(node.next);
+			node.unlink();
+		} else if (node != null && node === this) {
+			if (this.next != null) {
+				var next = this.next;
+				this.obj = this.next.obj;
+				this.linkNext(next.next);
+				next.unlink();
+			} else {
+				this.obj = null;
+			}
+		}
+	},
+
 	replace : function(list) {
 		
-	}
+	},
 
 }));
